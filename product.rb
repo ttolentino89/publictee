5e44f6b99a17c1baf85c717535c1a7ffc6e0b614
ttolentino89 <trishatolentino89@gmail.com>
2020-07-26 16:13:57 -0400

    mvp reached

--- product.rb
+++ product.rb
@@ -1,4 +1,6 @@
 class Product
+  # initialize empty array
+  @@all = []
 
   attr_reader  :product_type, :options
 
@@ -6,17 +8,35 @@ class Product
     @product_type = product_type
     @options = options
   end
+
+  def self.all
+    @@all.select { |product| product.is_a?(self)}
+  end
+
+  def self.getNewProduct(product_type, options)
+    if product_type == 'tshirt'
+      Tshirt.new(options['gender'], options['color'], options['size'])
+    elsif product_type == 'mug'
+      Mug.new(options['type'])
+    elsif product_type == 'sticker'
+      Sticker.new(options['size'], options['style'])
+    end
+  end
 end
 
 class Tshirt < Product
+
   attr_reader :gender, :color, :size
 
   def initialize(gender, color, size)
     @product_type = 'tshirt'
-    @options = { gender: gender, color: color, size: size}
+    @options = {gender: gender,
+                color: color,
+                size: size}
     @gender = gender
     @color = color
     @size = size
+    @@all << self
   end
 end
 
@@ -24,9 +44,10 @@ class Mug < Product
 attr_reader :type
 
   def initialize(type)
-    @product_type = 'Mug'
-    @options = { type: type }
+    @product_type = 'mug'
+    @options = {type: type}
     @type = type
+    @@all << self
   end
 end
 
@@ -35,8 +56,10 @@ class Sticker < Product
 
   def initialize(size, style)
     @product_type = 'sticker'
-    @options = { size: size, style: style }
+    @options = {size: size,
+                style: style}
     @size = size
     @style = style
+    @@all << self
   end
 end
--- testpublic.rb
+++ testpublic.rb
@@ -1,41 +1,93 @@
 require 'json'
 require_relative 'product.rb'
 
-# def getProducts
-  # file = File.read('products.json')
-  # products = JSON.parse(file)
-  # products= JSON.load(file)
-  # file.close
-
-  # products each do |product|
-  #   Product.getProducts(products['product_type'], products['options'])
-  # end
-# end
-
 def getItem
   file = File.read('products.json')
   products = JSON.parse(file)
+  products.each do |product|
+    Product.getNewProduct(product['product_type'], product['options'])
+  end
   puts 'Welcome to PublicTee! Which type of item are you looking for today?'
-  puts "Ex: 'Tshirt', 'Tshirt Male', 'Sticker',  etc."
+  puts "Ex: 'mug', 'tshirt male red', 'sticker',  etc."
   selection = gets.chomp
   options = selection.split(' ')
-  # puts products
-  # getProducts
-  puts  'Thank you for your selection!'
-  # puts "You have chosen #{options}"
-  puts "Here are your item's options:"
-  # if(products.key?(:options))
-  #   puts options
-  # # puts options
-  # else
-  #   puts 'Please enter a valid item!'
-  # end
+  puts "You have chosen #{selection}! Here's what we found:"
+  product_type = options[0]
+  if product_type == 'tshirt'
+    getShirtOptions(options)
+  elsif product_type == 'mug'
+    getMugOptions(options)
+  elsif product_type == 'sticker'
+    getStickerOptions(options)
+  end
+  puts 'Thank you and have a nice day :)'
 end
 
-# def findOptions(selection)
-#   if selection == 'tshirt'
-#
-# end
+def getShirtOptions(options)
+  #map through all tshirt options
+  genders = Tshirt.all.map(&:gender).uniq
+  colors = Tshirt.all.map(&:color).uniq
+  sizes = Tshirt.all.map(&:size).uniq
+  #split user selection subcategories into gender, color, size
+  selectedGender = options[1]
+  selectedColor = options[2]
+  selectedSize = options[3]
+  if selectedGender.nil? && selectedColor.nil? && selectedSize.nil?
+    puts "Genders: #{genders.join(', ')}"
+    puts "Colors: #{colors.join(', ')}"
+    puts "Sizes: #{sizes.join(', ')}"
+  elsif genders.include?(selectedGender) && colors.include?(selectedColor) && sizes.include?(selectedSize)
+    puts "It looks like there are no other options available."
+  elsif genders.include?(selectedGender) && selectedColor.nil? && selectedSize.nil?
+    puts "Colors: #{colors.join(', ')}"
+    puts "Sizes: #{sizes.join(', ')}"
+  elsif colors.include?(selectedColor) && selectedGender.nil? && selectedSize.nil?
+    puts "Genders: #{genders.join(', ')}"
+    puts "Sizes: #{sizes.join(', ')}"
+  elsif sizes.include?(selectedSize) && selectedGender.nil? && selectedColor.nil?
+    puts "Genders: #{genders.join(', ')}"
+    puts "Colors: #{colors.join(', ')}"
+  elsif genders.include?(selectedGender) && colors.include?(selectedColor) && selectedSize.nil?
+    puts "Sizes: #{sizes.join(', ')}"
+  elsif colors.include?(selectedColor) && sizes.include?(selectedSize) && selectedGender.nil?
+    puts "Genders: #{genders.join(', ')}"
+  elsif genders.include?(selectedGender) && sizes.include?(selectedSize) && selectedColor.nil?
+    puts "Colors: #{colors.join(', ')}"
+  else
+    puts "Tshirt options not found! Please try again :("
+    getItem
+  end
+end
+
+def getMugOptions(options)
+  types = Mug.all.map(&:type).uniq
+  selectedType = options[1]
+  if selectedType.nil?
+    puts "Type: #{types.join(', ')}"
+  elsif types.include?(selectedType)
+    puts "It looks like there are no other options available."
+  else
+    puts "Mug options not found! Please try again :("
+    getItem
+  end
+end
+
+def getStickerOptions(options)
+  sizes = Sticker.all.map(&:size).uniq
+  styles = Sticker.all.map(&:style).uniq
+  selectedSize = options[1]
+  selectedStyle = options[2]
+  if selectedSize.nil? && selectedStyle.nil?
+    puts "Sizes: #{sizes.join(', ')}"
+    puts "Styles: #{styles.join(', ')}"
+  elsif sizes.include?(selectedSize) && selectedStyle.nil?
+    puts "Styles: #{styles.join(', ')}"
+  elsif styles.include?(selectedStyle) && selectedSize.nil?
+    puts "Sizes: #{sizes.join(', ')}"
+  else
+    puts "Sticker options not found! Please try again :("
+    getItem
+  end
+end
 
 getItem
-# findOptions
